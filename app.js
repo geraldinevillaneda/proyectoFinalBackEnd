6162e09006e86c00f7d2761b90d77db5780eaa69
Geraldine Lopez <glopez@fivetech.co>
2021-04-15 04:19:36 -0500

    Backend geraldine grupos

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 testAPIRouter = require('./routes/testAPI');
 sedesRouter = require('./routes/sedes');
+gruposRouter = require('./routes/grupos');
 var app = express();
 
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
 app.use('/sedes', sedesRouter);
+app.use('/grupos',gruposRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
